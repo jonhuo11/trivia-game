8c30f891caf4680bcb6202c2712f32a9692b7383
Jonathan Huo <jonhuo11@gmail.com>
2024-02-03 18:08:27 -0500

    timer working

--- src/TriviaApp/Trivia/TriviaGame.tsx
+++ src/TriviaApp/Trivia/TriviaGame.tsx
@@ -1,138 +1,151 @@
-import { forwardRef, useContext, useEffect, useImperativeHandle, useRef, useState } from "react"
-import { RoomStateContext } from "../Room/Room"
-import TeamsList from "./TeamsList"
-import { Box, Button, Typography } from "@mui/material"
-import { TriviaGameActionMessage, TriviaGameUpdate } from "../Messages"
-import QuestionDisplay from "./QuestionDisplay"
-import { ObjectReplace } from "../../Util"
+import {
+	forwardRef,
+	useContext,
+	useImperativeHandle,
+	useState,
+} from "react";
+import { RoomStateContext } from "../Room/Room";
+import TeamsList from "./TeamsList";
+import { Box, Button } from "@mui/material";
+import { TriviaGameActionMessage, TriviaGameUpdate } from "../Messages";
+import { ObjectReplace } from "../../Util";
 
 enum TriviaState {
-    INLIMBO = 0,
-    INROUND = 1,
-    INLOBBY = 2
+	INLIMBO = 0,
+	INROUND = 1,
+	INLOBBY = 2,
 }
 
 interface TriviaGameState {
-    // lobby, in game, win, ...
-    state: TriviaState
+	// lobby, in game, win, ...
+	state: TriviaState;
 
-    // blue team
-    blueTeam: string[],
+	// round number
+	round: number;
 
-    // red team
-    redTeam: string[]
+	// blue team
+	blueTeam: string[];
+
+	// red team
+	redTeam: string[];
 }
 
-const initialTriviaGameState:TriviaGameState = {
-    state: TriviaState.INLOBBY,
-    blueTeam: [],
-    redTeam: []
-}
+const initialTriviaGameState: TriviaGameState = {
+	state: TriviaState.INLOBBY,
+	round: 0,
+	blueTeam: [],
+	redTeam: [],
+};
 
 interface TriviaGameProps {
-    wsSendGameMessage: (stringifiedContent:string) => void
-    startGame: () => void
+	wsSendGameMessage: (stringifiedContent: string) => void;
+	roomStartGame: () => void;
 }
 
 export interface TriviaGameHandle {
-    onServerTriviaGameUpdate: (update: TriviaGameUpdate) => void
-    reset: () => void // when the websocket disconnects
-    ping: () => void
+	onServerTriviaGameUpdate: (update: TriviaGameUpdate) => void;
+	reset: () => void; // when the websocket disconnects
+	ping: () => void;
 }
 
-const TriviaGame = forwardRef<TriviaGameHandle, TriviaGameProps>((
-    {wsSendGameMessage}: TriviaGameProps,
-    ref
-) => {
-    const roomState = useContext(RoomStateContext) // chat, playerlist, etc
-    const [gameState, setGameState] = useState(initialTriviaGameState)
+const TriviaGame = forwardRef<TriviaGameHandle, TriviaGameProps>(
+	({ wsSendGameMessage, roomStartGame }: TriviaGameProps, ref) => {
+		const roomState = useContext(RoomStateContext); // chat, playerlist, etc
+		const [gameState, setGameState] = useState(initialTriviaGameState);
 
-    // TODO reset gameState when disconnected or first connecting
+		// TODO reset gameState when disconnected or first connecting
 
-    const onWebsocketDisconnect = () => {
-        setGameState(initialTriviaGameState)
-    }
+		const onWebsocketDisconnect = () => {
+			setGameState(initialTriviaGameState);
+		};
 
-    const onServerTriviaGameUpdate = (update: TriviaGameUpdate):void => {
-        //console.log("Got trivia game update", update)
-        switch(gameState.state) {
-            case TriviaState.INLOBBY: {
-                /*
-                While in limbo, players can
-                - join blue/red team
+	    const onServerTriviaGameUpdate = (update: TriviaGameUpdate): void => {
+			// use cstate here to get current state value as setState calls are batched
+			setGameState((cstate) => {
+				if (update.state !== cstate.state) {
+					console.log(`Prev state ${cstate.state} New state ${update.state}`)
+				}
+				switch (cstate.state) {
+					case TriviaState.INLOBBY: {
+						/*
+                    While in limbo, players can
+                    - join blue/red team
 
-                While in round, players can
-                - vote
-                */
+                    While in round, players can
+                    - vote
+                    */
 
-                // TODO check here if gameState was changed by server
-                break
-            }
-            case TriviaState.INLIMBO: {
-                break
-            }
-            case TriviaState.INROUND: {
-                // TODO game logic
-                break
-            }
-        }
-        // update local state
-        setGameState(c => {
-            const newC = {...c}
-            ObjectReplace(newC, update)
-            //console.log(newC)
-            return newC
-        })
-    }
+						// TODO check here if gameState was changed by server
+						break;
+					}
+					case TriviaState.INLIMBO: {
+						break;
+					}
+					case TriviaState.INROUND: {
+						// TODO game logic
+						break;
+					}
+				}
+				const newC = { ...cstate };
+				ObjectReplace(newC, update);
+				return newC;
+			});
+		};
 
-    // passes the callback for when trivia game updates are received back to the room manager
-    useImperativeHandle(ref, () => {
-        return {
-            onServerTriviaGameUpdate,
-            reset() {
-                // when websocket disconnects
-                onWebsocketDisconnect()
-            },
-            ping() {
-                console.log("Pinged TriviaGame")
-            }
-        }
-    }, [])
+		// passes the callback for when trivia game updates are received back to the room manager
+		useImperativeHandle(
+			ref,
+			() => {
+				return {
+					onServerTriviaGameUpdate,
+					reset() {
+						// when websocket disconnects
+						onWebsocketDisconnect();
+					},
+					ping() {
+						console.log("Pinged TriviaGame");
+					},
+				};
+			},
+			[]
+		);
 
-    const joinTeam = (color: "blue" | "red") => {
-        const tgam:TriviaGameActionMessage = {
-            join: color === "blue" ? 0 : 1
-        }
-        wsSendGameMessage(JSON.stringify(tgam))
-    }
+		const joinTeam = (color: "blue" | "red") => {
+			const tgam: TriviaGameActionMessage = {
+				join: color === "blue" ? 0 : 1,
+			};
+			wsSendGameMessage(JSON.stringify(tgam));
+		};
 
-    const startGame = () => {
+		const startGame = () => {
+			roomStartGame();
+		};
 
-    }
-
-    return <Box>
-        {gameState.state === TriviaState.INLOBBY && <Box>
-            <TeamsList
-                blue={gameState.blueTeam}
-                red={gameState.redTeam}
-                handleClickBlueTeam={() => {
-                    joinTeam("blue")
-                }}
-                handleClickRedTeam={() => {
-                    joinTeam("red")
-                }}
-            />
-            <Button
-                variant="outlined"
-                sx={{
-                    
-                }}
-                onClick={startGame}
-            >
-                Start Game
-            </Button>
-        </Box>}
-        {/*
+		return (
+			<Box>
+				{gameState.state === TriviaState.INLOBBY && (
+					<Box>
+						<TeamsList
+							blue={gameState.blueTeam}
+							red={gameState.redTeam}
+							handleClickBlueTeam={() => {
+								joinTeam("blue");
+							}}
+							handleClickRedTeam={() => {
+								joinTeam("red");
+							}}
+						/>
+						<Button
+							variant="outlined"
+							sx={{
+								display: roomState.isOwner ? "flex" : "none",
+							}}
+							onClick={startGame}>
+							Start Game
+						</Button>
+					</Box>
+				)}
+				{/*
         <Box>
             <QuestionDisplay
                 q="Which voice actor does the voice for Quagmire?"
@@ -140,7 +153,9 @@ const TriviaGame = forwardRef<TriviaGameHandle, TriviaGameProps>((
             />
         </Box>
         */}
-    </Box>
-})
+			</Box>
+		);
+	}
+);
 
-export default TriviaGame
+export default TriviaGame;
--- src/Util.ts
+++ src/Util.ts
@@ -1,6 +1,7 @@
 export const ObjectReplace = (original:any, replacer:any) => {
     for (let key in replacer) {
         if (key in original && replacer[key] !== null && replacer[key] !== undefined) {
+            //console.log("Replaced in original", key)
             original[key] = replacer[key]
         }
     }
